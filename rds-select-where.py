78f3fe7f1b0a86a2a7344a8a28c994abbe0c5203
cloudtechtrainer <126149128+cloudtechtrainer@users.noreply.github.com>
2023-04-30 07:04:51 +0530

    Update rds-select-where.py

--- rds-select-where.py
+++ rds-select-where.py
@@ -1,11 +1,49 @@
 import mysql.connector
 
+import boto3
+from botocore.exceptions import ClientError
+
+
+def get_secret():
+
+    secret_name = "test/MySql"
+    region_name = "ap-northeast-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    secret = get_secret_value_response['SecretString']
+    
+    # Parse the secret JSON object
+    secret_dict = json.loads(secret)
+    username = secret_dict['username']
+    password = secret_dict['password']
+    host = secret_dict['host']
+    port = secret_dict['port']
+    database = secret_dict['database']
+
+    # Your code goes here.
+
 # Connect to the RDS MySQL database
 db = mysql.connector.connect(
-    host="database-1.cluster-ro-clk1drfaf3ne.ap-northeast-1.rds.amazonaws.com",
-    user="admin",
-    password="Abcd1234",
-    database="boopathi"
+    host=host,
+    user=username,
+    password=password,
+    database=database
 )
 
 # Read the input value from a file
