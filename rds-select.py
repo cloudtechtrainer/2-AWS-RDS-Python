96caa41c9311b33fea17c471566d33ce659c9f0c
cloudtechtrainer <126149128+cloudtechtrainer@users.noreply.github.com>
2023-04-30 09:30:54 +0530

    Update rds-select.py

--- rds-select.py
+++ rds-select.py
@@ -1,22 +1,39 @@
-import mysql.connector
+import boto3
+import json
+import pymysql
 
-# Connect to the RDS MySQL database
-db = mysql.connector.connect(
-  host="your-database-endpoint.rds.amazonaws.com",
-  user="your-username",
-  password="your-password",
-  database="your-database-name"
-)
+import os
+os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAV44TXIU47BQNCZON'
+os.environ['AWS_SECRET_ACCESS_KEY'] = 'BSSRwLfT7VM1SymLOUWK9oIL3lNWx2BMAPUF/tAC'
 
-# Execute a SELECT query
-cursor = db.cursor()
-query = "SELECT * FROM your-table-name"
-cursor.execute(query)
+# Specify the AWS region
+region = 'ap-northeast-2'
 
-# Write the results to a file
-with open('output.txt', 'w') as f:
-  for row in cursor:
-    f.write(str(row) + '\n')
+# Create a Secrets Manager client with the specified region
+secrets_client = boto3.client('secretsmanager', region_name=region)
+
+# Retrieve the secret value for your RDS database
+secret = secrets_client.get_secret_value(SecretId='test/MySqlSecret')
+
+# Parse the secret JSON string and extract the database credentials
+secret_dict = json.loads(secret['SecretString'])
+db_host = secret_dict['host']
+db_port = secret_dict['port']
+db_user = secret_dict['username']
+db_pass = secret_dict['password']
+db_name = secret_dict['dbname']
+
+# Connect to the RDS database using the extracted credentials
+conn = pymysql.connect(host=db_host, port=db_port, user=db_user, passwd=db_pass,db=db_name)
+
+# Execute a SQL query on the database
+cur = conn.cursor()
+cur.execute('SELECT * FROM my_table')
+results = cur.fetchall()
+
+# Print the query results
+for row in results:
+    print(row)
 
 # Close the database connection
-db.close()
+conn.close()
